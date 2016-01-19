737f474666c954a8e85e4805a4de8c340fb9cc55
MarkOfVerys <mark.stouffer@verys.com>
2016-01-19 14:46:46 -0800

    added some links.

--- app/async.js
+++ app/async.js
@@ -1,8 +1,15 @@
+// http://dailyjs.com/2014/02/20/promises-in-detail/
+// http://javascriptplayground.com/blog/2015/02/promises/
+// https://davidwalsh.name/promises
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+      value.then = function (val) { return true; }
+      value.done = function(doneVal) {return true;}
+      return value;
   },
 
   manipulateRemoteData : function(url) {
